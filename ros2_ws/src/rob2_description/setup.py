454bbf3e03049de2060825398758fabe87565d2b
Silly Goose <m.beni0621@gmail.com>
2026-04-28 16:08:04 +0200

    Add files via upload

---
+++ ros2_ws/src/rob2_description/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+import os
+from glob import glob
+
+package_name = 'rob2_description'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
+        (os.path.join('share', package_name, 'urdf'), glob('urdf/*')),
+        (os.path.join('share', package_name, 'meshes'), glob('meshes/*')),
+        (os.path.join('share', package_name, 'config'), glob('config/*')),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    entry_points={
+        'console_scripts': [
+            'wheel_controller = rob2_description.wheel_controller:main',
+        ],
+    },
+)
